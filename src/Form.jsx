832ab58d443ccc9de63e10b2080590193c0e5ce6
eis-Jeremy <eis08.jerry@gmail.com>
2024-07-02 16:30:49 +0200

    Color generator functionality completed

---
+++ src/Form.jsx
@@ -0,0 +1,38 @@
+import React, { useEffect, useState } from 'react'
+
+function Form({addColor}) {
+  const [color, setColor] = useState('')
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    addColor(color)
+  }
+
+  return (
+    <section className="container">
+      <h4>Color generator</h4>
+      <form className="color-form" onSubmit={handleSubmit}>
+        <input
+          type="color"
+          value={color}
+          onChange={(e) => setColor(e.target.value)}
+        />
+        <input
+          type="text"
+          value={color}
+          onChange={(e) => setColor(e.target.value)}
+          placeholder="#05ee07"
+        />
+        <button
+          type="submit"
+          className="btn"
+          style={{ background: color }}
+        >
+          Submit
+        </button>
+      </form>
+    </section>
+  )
+}
+
+export default Form
